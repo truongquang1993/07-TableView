079b24be05af34b41b7e86e550805cd790261b10
truong quang <truongquang9x@gmail.com>
2019-07-04 14:32:04 +0700

    commit1: create project

---
+++ 07-TableView/07-TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  07-TableView
+//
+//  Created by Trương Quang on 7/4/19.
+//  Copyright © 2019 Trương Quang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
